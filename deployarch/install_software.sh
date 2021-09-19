fb442573d45876c794c46f7c32d492f6cbd9ba53
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-09-19 18:10:57 +0300

    Update install_software.sh

--- deployarch/install_software.sh
+++ deployarch/install_software.sh
@@ -1,4 +1,10 @@
-cd /tmp
-curl -O https://raw.githubusercontent.com/LukasDoesDev/deployarch2/master/install_software.sh
-chmod +x install_software.sh
+#cd /tmp
+#curl -O https://raw.githubusercontent.com/LukasDoesDev/deployarch/master/install_software.sh
+#chmod +x install_software.sh
+#./install_software.sh
+
+curl -O https://codeload.github.com/LukasDoesDev/deployarch/zip/master
+pacman -Sy unzip python
+unzip master
+cd deployarch-master
 ./install_software.sh
